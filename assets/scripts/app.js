48e04c239290d7240ac86789929a89d3303d7e08
Riuseigi <troyliamjarata@gmail.com>
2022-09-18 17:30:52 +0800

    created reusable function for player attack

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,19 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE =14;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 adjustHealthBars(chosenMaxLife);
-function attackHandler(){
-    const damage = dealMonsterDamage(ATTACK_VALUE);
+
+function attackMonster(mode){
+    let maxDamnage;
+    if(mode === 'ATTACK'){
+        maxDamnage = ATTACK_VALUE;
+    } else if(mode === 'STRONG_ATTACK'){
+        maxDamnage = STRONG_ATTACK_VALUE;
+    }
+    const damage = dealMonsterDamage(maxDamnage);
     currentMonsterHealth -= damage;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
@@ -17,5 +25,11 @@ function attackHandler(){
         alert('You have a draw')
     }
 }
-
+function attackHandler(){
+    attackMonster('ATTACK');
+}
+function strongAttackHandler(){
+    attackMonster('STRONG_ATTACK')
+}
 attackBtn.addEventListener('click',attackHandler);
+strongAttackBtn.addEventListener('click',strongAttackHandler)
