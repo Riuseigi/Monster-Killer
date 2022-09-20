5279c316d835ccabce8d02fedd06e844b0d12cd8
Riuseigi <troyliamjarata@gmail.com>
2022-09-20 17:29:00 +0800

    added input to the health value of both sides

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,17 +5,19 @@ const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE =14;
 const HEAL_VALUE = 20;
-
 const MODE_ATTACK = 'ATTACK'
 const MODE_STRONG_ATTACK = 'STRONG_ATTACK'
 
 const enteredValue = prompt('Maximum life for you and the monster.', '100')
-let chosenMaxLife = 100;
+let chosenMaxLife = parseInt(enteredValue);
+if (isNaN(chosenMaxLife) || chosenMaxLife<=0){
+    chosenMaxLife = 100;
+}
 currentMonsterHealth = chosenMaxLife;
 currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
 
-adjustHealthBars(chosenMaxLife); // sets the healthbar to default 100 
+adjustHealthBars(chosenMaxLife); 
 function reset(){
     let currentMonsterHealth = chosenMaxLife;
     let currentPlayerHealth = chosenMaxLife;
