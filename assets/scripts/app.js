efa9f11240cdf02d4b5085de7b3e4ecee2f8e206
Riuseigi <troyliamjarata@gmail.com>
2022-09-25 19:21:54 +0800

    used break to show every log

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,16 +7,25 @@ const MONSTER_ATTACK_VALUE =14;
 const HEAL_VALUE = 20;
 const MODE_ATTACK = 'ATTACK'
 const MODE_STRONG_ATTACK = 'STRONG_ATTACK'
+
+
+/* A constant variable that is used to set the value of the attack, strong attack, monster attack and
+heal. */
 const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK';
 const LOG_EVENT_PLAYER_STRONG_ATTACK = 'PLAYER_STRONG ATTACK';
 const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
 const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
 const LOG_EVENT_GAME_OVER = 'GAME_OVER';
+
+
+/* Asking the user to enter a value, and then it is converting that value to an integer. */
 const enteredValue = prompt('Maximum life for you and the monster.', '100')
 let chosenMaxLife = parseInt(enteredValue);
+let lastLogEntry;
 if (isNaN(chosenMaxLife) || chosenMaxLife<=0){
     chosenMaxLife = 100;
 }
+
 currentMonsterHealth = chosenMaxLife;
 currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
@@ -238,9 +247,13 @@ function pirntLogHandler(){
     // console.log(battleLog);
     let i = 0;
     for (const logEntry of battleLog){
-        console.log(`#${i}`)
-        for(const key in logEntry){
-        console.log(`${key} => ${logEntry[key]}`);
+        if(!lastLogEntry && lastLogEntry !==0    || lastLogEntry < i){
+            console.log(`#${i}`)
+            for(const key in logEntry){
+            console.log(`${key} => ${logEntry[key]}`);
+            }
+            lastLogEntry = i;
+            break;
         }
         i++
     }
